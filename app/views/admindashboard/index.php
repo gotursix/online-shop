b43c0fa65e6e3c3f515a3e5447d3ab02dd215448
Popescu Valentin-Dimitrie <popescuvalentin16768@gmail.com>
2019-06-03 13:25:54 +0300

    nu gasesc ruah ca sa schimb cu euro click

--- app/views/admindashboard/index.php
+++ app/views/admindashboard/index.php
@@ -12,49 +12,52 @@
 <?php $this->start('body')?>
 
 <br>
-<h1>Permiteți utilizatorii pe forum</h1>
-<hr /><br>
+<div class="container">
+    <div class="row">
+        <h1>Permiteți utilizatorii pe forum</h1>
+        <hr /><br>
 
-<table class="table table-bordered table-hover table-striped table-sm text-center">
-    <thead>
-        <th>Creat la </th>
-        <th>Numele de utilizator</th>
-        <th>Email</th>
-        <th>Nume</th>
-        <th>Prenume</th>
-        <th></th>
-    </thead>
-    <tbody>
-        <?php foreach($this->users as $user): ?>
-        <?php if($user->acl !='["SuperAdmin"]'): ?>
-        <tr data-id="<?=$user->id?>">
-            <td><?=$user->created_at ?></td>
-            <td><?=$user->username ?></td>
-            <td><?=$user->email ?></td>
-            <td><?=$user->fname ?></td>
-            <td><?=$user->lname ?></td>
-            <?php if($user->acl =='["User"]'): ?>
-            <td>
-                <p class="text-important">Permis</p>
-            </td>
-            <?php else: ?>
-            <td>
-                <p class="text-danger">Respins</p>
-            </td>
-            <?php endif; ?>
-            <td class="text-center">
-                <a class="btn btn-sm btn-secondary mr-1" href="<?=PROOT?>admindashboard/changerank/<?=$user->id?>"><i class="fas fa-edit"></i></a>
-            </td>
-        </tr>
+        <table class="table table-bordered table-hover table-striped table-sm text-center">
+            <thead>
+                <th>Creat la </th>
+                <th>Numele de utilizator</th>
+                <th>Email</th>
+                <th>Nume</th>
+                <th>Prenume</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <?php foreach($this->users as $user): ?>
+                <?php if($user->acl !='["SuperAdmin"]'): ?>
+                <tr data-id="<?=$user->id?>">
+                    <td><?=$user->created_at ?></td>
+                    <td><?=$user->username ?></td>
+                    <td><?=$user->email ?></td>
+                    <td><?=$user->fname ?></td>
+                    <td><?=$user->lname ?></td>
+                    <?php if($user->acl =='["User"]'): ?>
+                    <td>
+                        <p class="text-important">Permis</p>
+                    </td>
+                    <?php else: ?>
+                    <td>
+                        <p class="text-danger">Respins</p>
+                    </td>
+                    <?php endif; ?>
+                    <td class="text-center">
+                        <a class="btn btn-sm btn-secondary mr-1" href="<?=PROOT?>admindashboard/changerank/<?=$user->id?>"><i class="fas fa-edit"></i></a>
+                    </td>
+                </tr>
 
-        <?php endif; ?>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+                <?php endif; ?>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
 
-<br><br><br><br><br><br>
-<h1>Anunțuri permise</h1>
-<hr /><br>
-
-<?php $this->end(); ?>
+        <br><br><br><br><br><br>
+        <h1>Anunțuri permise</h1>
+        <hr />
+    </div>
+</div>
+    <?php $this->end(); ?>
--- app/views/adminproducts/index.php
+++ app/views/adminproducts/index.php
@@ -1,6 +1,7 @@
 <?php use Core\{FH , H}; ?>
 <?php $this->start('body')?>
-
+<div class="container">
+    <div class="row">
 <?php
 $deleted = 0;
 $aproved = 0 ;
@@ -15,103 +16,114 @@ $aproved = 0 ;
 
 <?php if($deleted!=0): ?>
 <?php if($this->currentUser->acl != '["SuperAdmin"]'): ?>
-<h1 class="text-center">Pending for aprove</h1><hr />
-<table class="table table-bordered table-hover table-striped table-sm">
-  <thead>
-    <th>Nume</th>
-    <th>Preț</th>
-    <th>Transport</th>
-    <th></th>
-  </thead>
-  <tbody>
-    <?php foreach($this->products as $product): ?>
-      <?php if($product->deleted == 1): ?>
-      <tr data-id="<?=$product->id?>">
-        <td><?=$product->name ?></td>
-        <td><?=$product->price ?></td>
-        <td><?=$product->shipping ?></td>
-        <td class="text-right">
-          <a class="btn btn-sm btn-secondary mr-1" href="<?=PROOT?>adminproducts/edit/<?=$product->id?>"><i class="fas fa-edit"></i></a>
-          <a class="btn btn-sm btn-danger" href="#" onclick="deleteProduct('<?=$product->id?>');return false;"><i class="fas fa-trash-alt"></i></a>
-        </td>
-      </tr>
-       <?php endif; ?>
-    <?php endforeach; ?>
-  </tbody>
-</table>
-<br><br>
-<?php endif; ?>
-<?php endif; ?>
 
-<?php if($aproved !=0): ?>
-<h1 class="text-center">Anunțurile mele</h1><hr />
-<table class="table table-bordered table-hover table-striped table-sm">
-  <thead>
-    <th>Nume</th>
-    <th>Preț</th>
-    <th>Transport</th>
-    <th></th>
-  </thead>
-  <tbody>
-    <?php foreach($this->products as $product): ?>
-      <?php if($product->deleted == 0): ?>
-      <tr data-id="<?=$product->id?>">
-        <td><?=$product->name ?></td>
-        <td><?=$product->price ?></td>
-        <td><?=$product->shipping ?></td>
-        <td class="text-right">
-          <a class="btn btn-sm btn-outline-info mr-1" onclick="toggleFeatured('<?=$product->id?>');return false;">
-            <i data-id="<?=$product->id?>" class="<?=($product->featured == 1)? 'fas fa-star': 'far fa-star'?>"></i>
-          </a>
-          <a class="btn btn-sm btn-secondary mr-1" href="<?=PROOT?>adminproducts/edit/<?=$product->id?>"><i class="fas fa-edit"></i></a>
-          <a class="btn btn-sm btn-danger" href="#" onclick="deleteProduct('<?=$product->id?>');return false;"><i class="fas fa-trash-alt"></i></a>
-        </td>
-      </tr>
-    <?php endif; ?>
-    <?php endforeach; ?>
-  </tbody>
-</table>
-<?php endif; ?>
+        <h1 class="text-center">În așteptarea aprobării</h1>
+        <hr />
+        <table class="table table-bordered table-hover table-striped table-sm">
+            <thead>
+                <th>Nume</th>
+                <th>Preț</th>
+                <th>Transport</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <?php foreach($this->products as $product): ?>
+                <?php if($product->deleted == 1): ?>
+                <tr data-id="<?=$product->id?>">
+                    <td><?=$product->name ?></td>
+                    <td><?=$product->price ?></td>
+                    <td><?=$product->shipping ?></td>
+                    <td class="text-right">
+                        <a class="btn btn-sm btn-secondary mr-1" href="<?=PROOT?>adminproducts/edit/<?=$product->id?>"><i class="fas fa-edit"></i></a>
+                        <a class="btn btn-sm btn-danger" href="#" onclick="deleteProduct('<?=$product->id?>');return false;"><i class="fas fa-trash-alt"></i></a>
+                    </td>
+                </tr>
+                <?php endif; ?>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <br><br>
+        <?php endif; ?>
+        <?php endif; ?>
 
-<?php if($aproved ==0): ?>
-  <hr /><h1 class="text-center">Nici un anunț adăugat.</h1><hr />
-<?php endif; ?>
+        <?php if($aproved !=0): ?>
+        <h1 class="text-center">Anunțurile mele</h1>
+        <hr />
+        <table class="table table-bordered table-hover table-striped table-sm">
+            <thead>
+                <th>Nume</th>
+                <th>Preț</th>
+                <th>Transport</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <?php foreach($this->products as $product): ?>
+                <?php if($product->deleted == 0): ?>
+                <tr data-id="<?=$product->id?>">
+                    <td><?=$product->name ?></td>
+                    <td><?=$product->price ?></td>
+                    <td><?=$product->shipping ?></td>
+                    <td class="text-right">
+                        <a class="btn btn-sm btn-outline-info mr-1" onclick="toggleFeatured('<?=$product->id?>');return false;">
+                            <i data-id="<?=$product->id?>" class="<?=($product->featured == 1)? 'fas fa-star': 'far fa-star'?>"></i>
+                        </a>
+                        <a class="btn btn-sm btn-secondary mr-1" href="<?=PROOT?>adminproducts/edit/<?=$product->id?>"><i class="fas fa-edit"></i></a>
+                        <a class="btn btn-sm btn-danger" href="#" onclick="deleteProduct('<?=$product->id?>');return false;"><i class="fas fa-trash-alt"></i></a>
+                    </td>
+                </tr>
+                <?php endif; ?>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <?php endif; ?>
 
+        <?php if($aproved ==0): ?>
+        <hr />
+        <h1 class="text-center">Nici un anunț adăugat.</h1>
+        <hr />
+        <?php endif; ?>
+    </div>
+</div>
 <script>
-  function deleteProduct(id){
-    if(window.confirm("Sunteți sigur că doriți să ștergeți acest produs?")){
-      jQuery.ajax({
-        url : '<?=PROOT?>adminproducts/delete',
-        method : "POST",
-        data : {id : id},
-        success: function(resp){
-          var msgType = (resp.success)? 'success' : 'danger';
-          if(resp.success){
-            jQuery('tr[data-id="'+resp.model_id+'"]').remove();
-          }
-          alertMsg(resp.msg, msgType);
+    function deleteProduct(id) {
+        if (window.confirm("Sunteți sigur că doriți să ștergeți acest produs?")) {
+            jQuery.ajax({
+                url: '<?=PROOT?>adminproducts/delete',
+                method: "POST",
+                data: {
+                    id: id
+                },
+                success: function(resp) {
+                    var msgType = (resp.success) ? 'success' : 'danger';
+                    if (resp.success) {
+                        jQuery('tr[data-id="' + resp.model_id + '"]').remove();
+                    }
+                    alertMsg(resp.msg, msgType);
+                }
+            });
         }
-      });
     }
-  }
 
-  function toggleFeatured(id){
-    jQuery.ajax({
-      url: '<?=PROOT?>adminproducts/toggleFeatured',
-      method: "POST",
-      data : {id : id},
-      success : function(resp) {
-        if(resp.success){
-          var el = jQuery('i[data-id="'+resp.model_id+'"]');
-          var klass = (resp.featured)? 'fas' : 'far';
-          el.removeClass("fas far");
-          el.addClass(klass);
-          alertMsg(resp.msg,'success');
-        } else {
-          alertMsg(resp.msg, 'danger');
-        }
-      }
-    });
-  }
+    function toggleFeatured(id) {
+        jQuery.ajax({
+            url: '<?=PROOT?>adminproducts/toggleFeatured',
+            method: "POST",
+            data: {
+                id: id
+            },
+            success: function(resp) {
+                if (resp.success) {
+                    var el = jQuery('i[data-id="' + resp.model_id + '"]');
+                    var klass = (resp.featured) ? 'fas' : 'far';
+                    el.removeClass("fas far");
+                    el.addClass(klass);
+                    alertMsg(resp.msg, 'success');
+                } else {
+                    alertMsg(resp.msg, 'danger');
+                }
+            }
+        });
+    }
+
 </script>
 <?php $this->end(); ?>
